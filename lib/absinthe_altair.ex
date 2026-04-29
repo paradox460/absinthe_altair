373f3dfb9ad224b1c78b795708a1ec2460677a01
Jeff Sandberg <jeff@pdx.su>
2026-04-28 21:38:47 -0600

    feat: initial project scaffold

---
+++ lib/absinthe_altair.ex
@@ -0,0 +1,18 @@
+defmodule AbsintheAltair do
+  @moduledoc """
+  Documentation for `AbsintheAltair`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AbsintheAltair.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/absinthe_altair_test.exs
@@ -0,0 +1,8 @@
+defmodule AbsintheAltairTest do
+  use ExUnit.Case
+  doctest AbsintheAltair
+
+  test "greets the world" do
+    assert AbsintheAltair.hello() == :world
+  end
+end
